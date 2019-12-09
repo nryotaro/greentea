1d45de60f859a42c1b1b8fa0092298d165c55176
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2019-12-09 18:40:49 +0900

    implement create_tracer

--- greentea/trace.py
+++ greentea/trace.py
@@ -1,6 +1,7 @@
 """Provide methods to trace execution."""
 import logging
 import functools
+from typing import Callable
 
 
 class Tracer:
@@ -13,7 +14,7 @@ class Tracer:
         self.logger = logger
         self.log_level = log_level
 
-    def trace(self, function):
+    def trace(self, function: Callable):
         """Provide a function decorator to trace the decorated function."""
         @functools.wraps(function)
         def wrapper(*args, **kwargs):
@@ -26,3 +27,24 @@ class Tracer:
 
     def _log(self, message):
         self.logger.log(self.log_level, message)
+
+    @classmethod
+    def create_tracer(
+            cls,
+            logger_name: str = 'trace',
+            format: str = '%(levelname)s:%(asctime)s:%(name)s:%(message)s',
+            level: int = logging.DEBUG):
+        """Create a tracer named with `logger_name`.
+
+        Returns
+        -------
+        tracer: Tracer
+
+        """
+        handler = logging.StreamHandler()
+        handler.setLevel(level)
+        handler.setFormatter(logging.Formatter(format))
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(level)
+        logger.addHandler(handler)
+        return Tracer(logger, level)
--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock, call, patch
 import logging
 import greentea.trace as t
 
@@ -28,3 +28,34 @@ class TestTracer(TestCase):
             self.logger.log.call_args_list,
             [call(self.log_level, calling),
              call(self.log_level, f"{calling} -> 42")])
+
+    @patch('logging.Formatter', spec=logging.Formatter)
+    @patch('logging.getLogger', spec=logging.Logger)
+    @patch('logging.StreamHandler', spec=logging.StreamHandler)
+    def test_create_tracer(
+            self,
+            stream_handler,
+            get_logger,
+            formatter_constructor):
+        name = 'tracing'
+        formatter_str = '%(levelname)s:%(asctime)s:%(name)s:%(message)s'
+        level = logging.DEBUG
+        handler = stream_handler.return_value
+        formatter = formatter_constructor.return_value
+        logger = get_logger.return_value
+
+        tracer = t.Tracer.create_tracer(name, formatter_str, level)
+
+        self.assertIsInstance(tracer, t.Tracer)
+
+        self.assertEqual(get_logger.call_args_list, [call(name)])
+        self.assertEqual(stream_handler.call_args_list, [call()])
+        self.assertEqual(formatter_constructor.call_args_list,
+                         [call(formatter_str)])
+        self.assertEqual(
+            handler.setFormatter.call_args_list,
+            [call(formatter)])
+        self.assertEqual(handler.setLevel.call_args_list, [call(level)])
+        self.assertEqual(logger.addHandler.call_args_list, [call(handler)])
+        self.assertEqual(tracer.logger, logger)
+        self.assertEqual(tracer.log_level, level)

d72f6608b29592fec05c9378e96119d8c6d2eaba
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-01-06 01:31:07 +0900

    to 3.0.0

---
+++ tests/test_first_class_collection.py
@@ -0,0 +1,21 @@
+import os.path
+from unittest import TestCase
+import greentea.first_class_collection as fcc
+
+
+class Double(fcc.FirstClassFileContextManagerProvider):
+
+    def transform(self, item):
+        return len(item)
+
+
+class TestFirstClassFileContextManagerProvider(TestCase):
+
+    def test_call(self):
+        module_dir = os.path.dirname(__file__)
+        testdata = os.path.join(module_dir,
+                                'first_class_collection_call_test.txt')
+        target = Double(testdata)
+
+        with target() as stream:
+            self.assertEqual([3, 5], list(stream))

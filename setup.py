2fb16a0575b387c4ddd6be63f9836fb88594326b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-02-11 18:23:34 +0900

    downgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='greentea',
-      version="3.5.1",
+      version="3.5.2",
       description=(
           'A microframework for abstraction.'),
       python_requires='>=3.7.0',

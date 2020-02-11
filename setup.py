4c61fa576aa622d3a98432c32767d3c5fe4883d5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-02-11 18:16:45 +0900

    3.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='greentea',
-      version="3.5.0",
+      version="3.5.1",
       description=(
           'A microframework for abstraction.'),
       python_requires='>=3.8.0',
